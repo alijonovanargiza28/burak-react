69307a99f84666d3ecc7a9b231669926fee521f7
Nargiza <alijonovanargiza38@gmail.com>
2026-06-25 04:11:06 +0500

    fix:MUI cutomized integration

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,17 +2,21 @@ import React from "react";
 import "../css/app.css";
 
 import { Container, Stack, Box, Typography, Button } from "@mui/material";
+import { RippleBadge } from "./material/MaterialTheme/styled";
 
 function App() {
   return (
-    <Container maxWidth="sm">
+    <Container sx={({background:"orange"})}>
       <Stack direction="column">
         <Box sx={{ my: 4 }}>
           <Typography variant="h4" component="h4">
             Create React App on Typescript with REDUX
           </Typography>
         </Box>
-        <Button variant="contained">Contained</Button>
+        <Box>
+          <RippleBadge badgeContent={4}> <Button variant="contained">Contained</Button></RippleBadge>
+        </Box>
+       
       </Stack>
     </Container>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import theme from "./theme";
-
 import { store } from "./app/store";
 import App from "./app/App";
 import reportWebVitals from "./reportWebVitals";
-
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-//import theme from "./app/material/MaterialTheme";
+import theme from "./app/material/MaterialTheme";
 import "./css/index.css";
 
 const root = ReactDOM.createRoot(
