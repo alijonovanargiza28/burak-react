a3027627857bd23603c42b40877f70db9f8e0214
Nargiza <alijonovanargiza38@gmail.com>
2026-06-26 13:34:24 +0500

    feat:creat routing system

--- src/index.tsx
+++ src/index.tsx
@@ -8,9 +8,10 @@ import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import theme from "./app/material/MaterialTheme";
 import "./css/index.css";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 
 root.render(
@@ -18,10 +19,12 @@ root.render(
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <App />
+        <Router>
+          <App />
+        </Router>
       </ThemeProvider>
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 reportWebVitals();
