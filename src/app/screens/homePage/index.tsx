92382352f404094cca371ee36169719371be1025
Nargiza <alijonovanargiza38@gmail.com>
2026-06-29 15:42:47 +0500

    feat: complated homework

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { Container } from "@mui/material";
 import Statistics from "./Statistics";
 import PopularDishes from "./PopularDishes";
 import NewDishes from "./NewDishes";
 import Advertisement from "./Advertisement";
 import ActiveUsers from "./ActiveUsers";
 import Events from "./Events";
+import "../../../css/home.css"
 
 export default function HomePage() {
   return (
