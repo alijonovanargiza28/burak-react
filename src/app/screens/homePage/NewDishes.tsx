7d1c47844ae389afa7e6f4477ef46c36861a094d
Nargiza <alijonovanargiza38@gmail.com>
2026-07-01 12:00:47 +0500

    fix:modify popular and new dishes components

--- src/app/screens/homePage/NewDishes.tsx
+++ src/app/screens/homePage/NewDishes.tsx
@@ -20,46 +20,57 @@ export default function NewDishes() {
 
           <Stack className="cards-frame" direction="row" spacing={2}>
             <CssVarsProvider>
-              {newDishes.map((ele, index) => (
-                <Card key={index} variant="outlined" className="card">
-                  <CardOverflow>
-                    <div className="product-sale">Normal size</div>
+              {newDishes.length !== 0 ? (
+                newDishes.map((ele, index) => (
+                  <Card key={index} variant="outlined" className="card">
+                    <CardOverflow>
+                      <div className="product-sale">Normal size</div>
 
-                    <AspectRatio ratio="1">
-                      <img src={ele.imagePath} alt={ele.productName} />
-                    </AspectRatio>
-                  </CardOverflow>
+                      <AspectRatio ratio="1">
+                        <img src={ele.imagePath} alt={ele.productName} />
+                      </AspectRatio>
+                    </CardOverflow>
 
-                  <CardOverflow variant="soft" className="product-detail">
-                    <Stack className="info">
-                      <Stack
-                        direction="row"
-                        alignItems="center"
-                        justifyContent="space-between"
-                      >
-                        <Typography className="title">
-                          {ele.productName}
+                    <CardOverflow variant="soft" className="product-detail">
+                      <Stack className="info">
+                        <Stack
+                          direction="row"
+                          alignItems="center"
+                          justifyContent="space-between"
+                        >
+                          <Typography className="title">
+                            {ele.productName}
+                          </Typography>
+
+                          <Divider
+                            width="2"
+                            height="24"
+                            bg="#d9d9d9"
+                          />
+
+                          <Typography className="price">$12</Typography>
+                        </Stack>
+
+                        <Typography
+                          className="views"
+                          display="flex"
+                          alignItems="center"
+                        >
+                          20
+                          <VisibilityIcon
+                            sx={{
+                              fontSize: 20,
+                              marginLeft: "5px",
+                            }}
+                          />
                         </Typography>
-
-                        <Divider width="2" height="24" bg="#d9d9d9" />
-
-                        <Typography className="price">$12</Typography>
                       </Stack>
-
-                      <Typography
-                        className="views"
-                        display="flex"
-                        alignItems="center"
-                      >
-                        20
-                        <VisibilityIcon
-                          sx={{ fontSize: 20, marginLeft: "5px" }}
-                        />
-                      </Typography>
-                    </Stack>
-                  </CardOverflow>
-                </Card>
-              ))}
+                    </CardOverflow>
+                  </Card>
+                ))
+              ) : (
+                <Box className="no-data">No products found!</Box>
+              )}
             </CssVarsProvider>
           </Stack>
         </Stack>
